da62d059fb7b4a45557b2134c2f43911695bebf3
Ali-Yousaf <ali.yousaaf121@gmail.com>
2026-07-29 05:13:51 +0500

    screen fade added

--- Assets/Scripts/DeathFromTiles.cs
+++ Assets/Scripts/DeathFromTiles.cs
@@ -1,12 +1,24 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathFromTiles : MonoBehaviour
 {
+    [SerializeField] private float sceneTransitionTime;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            print("PLAYED DIED");
+            ScreenFade.Instance.Fade(sceneTransitionTime);
+            StartCoroutine(LoadScene());
         }
-    }
+    }   
+
+    private IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(sceneTransitionTime);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    } 
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public static GameManager Instance;
+
+    void Awake()
+    {
+        if(Instance == null)
+            Instance = this;
+
+        else
+            Destroy(gameObject);
+    }
 }
