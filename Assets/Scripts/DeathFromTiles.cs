ab0a0180d9550e74f8222180e9b51497d7365fcf
Ali-Yousaf <ali.yousaaf121@gmail.com>
2026-07-30 20:56:14 +0500

    added tiles

--- Assets/Scripts/DeathFromTiles.cs
+++ Assets/Scripts/DeathFromTiles.cs
@@ -7,12 +7,14 @@ public class DeathFromTiles : MonoBehaviour
 {
     [SerializeField] private float sceneTransitionTime;
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
+            print("Death");
+            
             if(LevelLoader.Instance != null)
                 LevelLoader.Instance.RestartLevel();
         }
-    }   
+    }
 }
