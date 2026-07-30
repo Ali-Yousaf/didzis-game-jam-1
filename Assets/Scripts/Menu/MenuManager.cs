0a0bed3f50cd1dbdce744c167ba23f17303b8c46
Ali-Yousaf <ali.yousaaf121@gmail.com>
2026-07-30 06:23:40 +0500

    added player settings and menu manager

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -3,8 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject gameRulesPanel;
+
+    void Start()
+    {
+        gameRulesPanel.SetActive(false);
+    }
+
     public void StartGame()
     {
         LevelLoader.Instance.LoadNextLevel();
+    }
+
+    public void OpenRulesPanel()
+    {
+        gameRulesPanel.SetActive(true);
+    }
+
+    public void CloseRulesPanel()
+    {
+        gameRulesPanel.SetActive(false);
     }
 }
